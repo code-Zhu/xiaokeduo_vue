31edfb3477a1530fb3d59216c2c85ef0ab3552c3
zhudongxi <1604356394@qq.com>
2019-01-25 17:56:18 +0800

    添加新商品，暂时逻辑还有问题

---
+++ src/pages/goods/components/Step1.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="step1">
+    <div class="step-list">
+      <ul class="list">
+        <li v-for="v in categories" :key="v.id"
+          @click="handleList(v, 0)" :class="{active: current_name === v.name}">
+          <span>{{v.name}}</span>
+          <i v-if="v.hasChildren" class="el-icon-caret-right"></i>
+        </li>
+      </ul>
+      <ul class="list" v-show="hasChildren">
+        <li v-for="v in child_list" :key="v.id"
+          @click="handleList(v, 1)" :class="{active: current_name === v.name}">
+          <span>{{v.name}}</span>
+          <i v-if="v.hasChildren" class="el-icon-caret-right"></i>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      categories: [
+        {
+          name: '新鲜水果',
+          hasChildren: true,
+          children: [
+            {
+              name: '苹果',
+              hasChildren: false
+            },
+            {
+              name: '香蕉',
+              hasChildren: false
+            }
+          ]
+        },
+        {
+          name: '儿童服饰',
+          hasChildren: true,
+          children: [
+            {
+              name: '男童',
+              hasChildren: false
+            },
+            {
+              name: '女童',
+              hasChildren: false
+            }
+          ]
+        },
+        {
+          name: '投资理财',
+          hasChildren: false
+        },
+        {
+          name: '药材',
+          hasChildren: false
+        }
+      ],
+      child_list: [],
+      hasChildren: false,
+      current_name: '',
+      step1Data: {
+        str: [],
+        continue: false
+      }
+    }
+  },
+  methods: {
+    handleList (val, level) {
+      if (level === 0) {
+        this.hasChildren = val.hasChildren
+      }
+      if (val.children) {
+        this.child_list = val.children
+        this.step1Data.continue = level === 1
+      } else {
+        // this.child_list = []
+        this.step1Data.continue = true
+      }
+      this.current_name = val.name
+      this.step1Data.str[level] = val.name
+      this.$emit('EventGetData', this.step1Data)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.step1{
+  border: 1px solid #C2DAF2;
+  background-color: #EFF8FF;
+  padding: 5px;
+  .step-list{
+    min-height: 250px;
+    background-color: #E5F0FF;
+    border: 1px solid #B1D2FF;
+    display: flex;
+    .list{
+      width: 190px;
+      background-color: #fff;
+      margin-right: 10px;
+      padding: 10px;
+      border-right: 1px solid #B1D2FF;
+      &+.list{
+        border-left: 1px solid #B1D2FF;
+      }
+      li{
+        line-height: 22px;
+        display: flex;
+        justify-content: space-between;
+        cursor: pointer;
+        padding-left: 10px;
+        &.active{
+          border: 1px solid #98BBD1;
+          background-color: #D6EDF4;
+        }
+        i{
+          line-height: 22px;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/pages/goods/selectcategory.vue
+++ src/pages/goods/selectcategory.vue
@@ -1,51 +1,68 @@
 <template>
   <div class="selectcategory">
     <p class="page-title">上架新商品</p>
+    <div class="pannel step-header">
+      <a href="javascript:;" :class="{finished: step>0}">1.选择商品分类</a>
+      <a href="javascript:;" :class="{finished: step>1}">2.编辑商品信息</a>
+      <a href="javascript:;" :class="{finished: step>2}">3.编辑商品详情</a>
+    </div>
+    <div class="pannel step-content">
+      <step1 @EventGetData="getStep1"/>
+    </div>
+    <div class="pannel step-footer">
+      <p>你当前选择的是：</p>
+      <p>
+        <el-button type="success" size="mini">已选好分类，进入下一步</el-button>
+      </p>
+    </div>
   </div>
 </template>
 <script>
+const Step1 = () => import('./components/Step1')
 export default {
+  components: {
+    Step1
+  },
   data () {
     return {
-      categories: [
-        {
-          name: '新鲜水果',
-          hasChildren: true,
-          children: [
-            {
-              name: '苹果',
-              hasChildren: false
-            },
-            {
-              name: '香蕉',
-              hasChildren: false
-            }
-          ]
-        },
-        {
-          name: '儿童服饰',
-          hasChildren: true,
-          children: [
-            {
-              name: '男童',
-              hasChildren: false
-            },
-            {
-              name: '女童',
-              hasChildren: false
-            }
-          ]
-        },
-        {
-          name: '投资理财',
-          hasChildren: false
-        },
-        {
-          name: '药材',
-          hasChildren: false
-        }
-      ]
+      step: 1,
+      step1Date: null
+    }
+  },
+  methods: {
+    getStep1 ($e) {
+      console.log($e)
     }
   }
 }
 </script>
+<style lang="scss" scoped>
+.step-header{
+  display: flex;
+  a{
+    padding: 10px 15px;
+    &.finished{
+      color: #555;
+      border-bottom: 2px solid #1CA47D;
+    }
+  }
+}
+.step-content{
+  margin: 10px 0;
+}
+.step-footer{
+  p:first-child{
+    padding: 0 15px;
+    font-size: 16px;
+    color: #31708F;
+    line-height: 40px;
+    background-color: #D9EDF7;
+    border-radius: 4px;
+    border: 1px solid #BCE8F1;
+    margin-bottom: 20px;
+  }
+  p~p{
+    text-align: center;
+  }
+}
+</style>
